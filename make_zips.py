2a3f5957ad26dccee90c8ed581320f6057db2ee9
Justin99x <allredjustin@hotmail.com>
2025-01-01 19:13:49 -0800

    Add sdkmod file and remove .idea/ files

--- make_zips.py
+++ make_zips.py
@@ -4,6 +4,7 @@ import os
 
 def zip_dir(dir_name: str):
     zip_file_name = os.path.join(dir_name, f"{dir_name}.zip")
+    sdkmod_name = os.path.join(dir_name, f"{dir_name}.sdkmod")
     zip_file = zipfile.ZipFile(zip_file_name, mode='w')
 
     try:
@@ -15,7 +16,13 @@ def zip_dir(dir_name: str):
     finally:
         zip_file.close()
 
+    os.rename(zip_file_name, sdkmod_name)
+
+
 
 if __name__ == '__main__':
-    # zip_dir("SpeedrunPractice")
-    zip_dir("BorderlandsRewind")
+
+    zip_dir("speedrun_practice")
+
+
+
